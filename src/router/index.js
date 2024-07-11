36eeeadf3791c02dc7254fa6203ff4a35f365926
imyb <imyb929@naver.com>
2024-07-11 16:43:49 +0900

    createWebHashHistory 로 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import sidebarNavItems from '@/router/sidebarNavItems'
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from 'vue-router';
+import sidebarNavItems from '@/router/sidebarNavItems';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      redirect: '/button'
+      redirect: '/button',
     },
-    ...sidebarNavItems
-  ]
-})
+    ...sidebarNavItems,
+  ],
+});
 
-export default router
+export default router;
